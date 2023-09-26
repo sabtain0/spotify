96f4da513476f018d02c2fdfc24e0a11a4bd6aae
sabtain0 <138502598+sabtain0@users.noreply.github.com>
2023-09-27 02:06:10 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -5,16 +5,16 @@ let proressbar = document.getElementById('progressbar');
 let gif = document.getElementById('gif')
 // let songitem = Array.from(document.getElementsByClassName('songItem'));
 let songs = [
-    {songname : "295" , filepath:"songs/1.mp3" , coverpath:"songs/1.jpg"},
-    {songname : "Badfella" , filepath:"songs/2.mp3" , coverpath:"songs/2.jpg"},
-    {songname : "Bitch I am back" , filepath:"songs/3.mp3" , coverpath:"songs/3.jpg"},
-    {songname : "Chorni" , filepath:"songs/4.mp3" , coverpath:"songs/4.jpg"},
-    {songname : "Dawood" , filepath:"songs/5.mp3" , coverpath:"songs/5.jpg"},
-    {songname : "Goat" , filepath:"songs/6.mp3" , coverpath:"songs/6.jpg"},
-    {songname : "G shit" , filepath:"songs/7.mp3" , coverpath:"songs/7.jpg"},
-    {songname : "Old skoolWe rollin" , filepath:"songs/8.mp3" , coverpath:"songs/8.jpg"},
-    {songname : "Vaar" , filepath:"songs/9.mp3" , coverpath:"songs/9.jpg"},
-    {songname : "" , filepath:"songs/10.mp3" , coverpath:"songs/10.jpg"},
+  { songname: "295", filepath: "songs/1.mp3", coverpath: "songs/1.jpg" },
+  { songname: "Badfella", filepath: "songs/2.mp3", coverpath: "songs/2.jpg" },
+  { songname: "Bitch I am back", filepath: "songs/3.mp3", coverpath: "songs/3.jpg" },
+  { songname: "Chorni", filepath: "songs/4.mp3", coverpath: "songs/4.jpg" },
+  { songname: "Dawood", filepath: "songs/5.mp3", coverpath: "songs/5.jpg" },
+  { songname: "Goat", filepath: "songs/6.mp3", coverpath: "songs/6.jpg" },
+  { songname: "G shit", filepath: "songs/7.mp3", coverpath: "songs/7.jpg" },
+  { songname: "Old skool", filepath: "songs/8.mp3", coverpath: "songs/8.jpg" },
+  { songname: "Vaar", filepath: "songs/9.mp3", coverpath: "songs/9.jpg" },
+  { songname: "We rollin", filepath: "songs/10.mp3", coverpath: "songs/10.jpg" },
 ]
 // songitem.forEach((element , i) => {
 //     console.log(element , i)
@@ -22,67 +22,80 @@ let songs = [
 //     element.getElementsByClassName('songname')[0].innertext =songs[i].songname
 // })
 
-masterplay.addEventListener ('click' , ()=>{
-      if(audio.paused || audio.currentTime<=0){
-        audio.play()
-        masterplay.classList.replace('fa-circle-play' , 'fa-circle-pause') 
-        gif.style.opacity = 1
-      }
-     
-      else{
-        audio.pause()
-        masterplay.classList.replace('fa-circle-pause' , 'fa-circle-play') 
-        gif.style.opacity = 0
-      }
+masterplay.addEventListener('click', () => {
+  if (audio.paused || audio.currentTime <= 0) {
+    audio.play()
+    masterplay.classList.replace('fa-circle-play', 'fa-circle-pause')
+    gif.style.opacity = 1
+  }
+
+  else {
+    audio.pause()
+    masterplay.classList.replace('fa-circle-pause', 'fa-circle-play')
+    gif.style.opacity = 0
+  }
 })
 
-audio.addEventListener('timeupdate' ,() =>{
-     progress = parseInt((audio.currentTime/audio.duration)*100)
-     console.log(progress)
-     proressbar.value = progress
+audio.addEventListener('timeupdate', () => {
+  progress = parseInt((audio.currentTime / audio.duration) * 100)
+  // console.log(progress)
+  proressbar.value = progress
 })
-proressbar.addEventListener('change' , ()=>{
-    audio.currentTime = proressbar.value * audio.duration/100
+proressbar.addEventListener('change', () => {
+  audio.currentTime = proressbar.value * audio.duration / 100
 })
-const makeAllPlays= (()=>{
+const makeAllPlays = (() => {
   Array.from(document.getElementsByClassName('songItem')).forEach((element) => {
-    element.classList.replace('fa-circle-play' , 'fa-circle-pause')
+    element.classList.replace('fa-circle-play', 'fa-circle-pause')
   })
 })
 
 Array.from(document.getElementsByClassName('songplay')).forEach((element) => {
-  element.addEventListener('click',(e)=>{
-     makeAllPlays()
-     songIndex = parseInt(e.target.id)
-     e.target.classList.replace('fa-circle-play' , 'fa-circle-pause');
-     audio.src = `${songIndex+1}.mp3`
-     audio.currentTime = 0;
-     audio.play();
-     masterplay.classList.replace('fa-circle-play' , 'fa-circle-pause') 
+  element.addEventListener('click', (e) => {
+    makeAllPlays()
+    songIndex = parseInt(e.target.id)
+    e.target.classList.replace('fa-circle-play', 'fa-circle-pause');
+    audio.src = `${songIndex + 1}.mp3`
+    audio.currentTime = 0;
+    audio.play();
+    masterplay.classList.replace('fa-circle-play', 'fa-circle-pause')
 
   })
 })
-document.getElementById('next').addEventListener('click',()=>{
-  if(songIndex>=9){
+document.getElementById('next').addEventListener('click', () => {
+  if (songIndex >= 9) {
     songIndex = 0
   }
-  else{
+  else {
     songIndex += 1
-    audio.src = `${songIndex+1}.mp3`
+    audio.src = `${songIndex + 1}.mp3`
     audio.currentTime = 0;
     audio.play();
-    masterplay.classList.replace('fa-circle-play' , 'fa-circle-pause')
+    masterplay.classList.replace('fa-circle-play', 'fa-circle-pause')
   }
 })
-document.getElementById('previous').addEventListener('click',()=>{
-  if(songIndex<=1){
+document.getElementById('previous').addEventListener('click', () => {
+  if (songIndex <= 1) {
     songIndex = 0
   }
-  else{
+  else {
     songIndex -= 1
-    audio.src = `${songIndex+1}.mp3`
+    audio.src = `${songIndex + 1}.mp3`
     audio.currentTime = 0;
     audio.play();
-    masterplay.classList.replace('fa-circle-play' , 'fa-circle-pause')
+    masterplay.classList.replace('fa-circle-play', 'fa-circle-pause')
+  }
+})
+document.getElementById('pausedddd').addEventListener('click', () => {
+  if (audio.paused || audio.currentTime <= 0) {
+    audio.src = `${songIndex + 1}.mp3`
+    audio.currentTime = 0;
+    audio.play();
+  }
+  else {
+    audio.src = `${songIndex+1}.mp3`
+     audio.currentTime = 0;
+     audio.pause();
+    // masterplay.classList.replace('fa-circle-pause', 'fa-circle-play')
   }
 })
